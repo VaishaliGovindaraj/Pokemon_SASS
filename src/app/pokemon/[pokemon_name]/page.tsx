77b51cf65743388ebc7baa5969a35656c60d942d
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-05-26 22:10:40 +0200

    upload

--- src/app/pokemon/[pokemon_name]/page.tsx
+++ src/app/pokemon/[pokemon_name]/page.tsx
@@ -1,27 +1,31 @@
 'use client'
 // import Types from "@/app/types/page";
 // import { useRouter } from "next/navigation";
+import React from "react";
 import DisplayPokemon from "@/components/DisplayPokemon";
 import { PokemonType } from "@/utils/types";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 
-interface PageProps {
-  params: {
-    pokemon_name: string;
-  };
-}
+type PageProps = Promise<{pokemon_name: string}>;
+//   params: {
+//     pokemon_name: string;
+//   };
+
 
 // const PokemonDetail =  ({params} : PageProps) => {
 
-export default function PokemonDetail ({params} : PageProps){
-    const {pokemon_name} =  params;
+export default async function PokemonDetail ({params} : {params: PageProps}){
+    const {pokemon_name} =  await params;
     // const router = useRouter();
 
     const [caughtPokemon, setCaughtPokemon] = useState<PokemonType | null>(null)
     // const [TypePage,setTypePage] = useState<boolean>(false);
 
-    const fetchPokemon = async (): Promise<void> => {
+        useEffect( () => {
+        
+    
+        const fetchPokemon = async (): Promise<void> => {
         try {
             const API_URL: string = "https://pokeapi.co/api/v2"
             const response = await fetch(`${API_URL}/pokemon/${pokemon_name}/`)
@@ -42,16 +46,15 @@ export default function PokemonDetail ({params} : PageProps){
         }
     }
 
-    const handleClick = () => {
-        setCaughtPokemon(null);
-        
-        // setTypePage(true);
-    }
+      },[])
 
-    useEffect( () => {
-        fetchPokemon();
-    },[])
-    
+    // const handleClick = () => {
+    //     setCaughtPokemon(null);
+        
+    //     // setTypePage(true);
+    // }
+
+
     return (
         <div className="pokemon_container">
             {caughtPokemon && <DisplayPokemon {...caughtPokemon}/>}
